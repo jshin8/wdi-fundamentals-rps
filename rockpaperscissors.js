277a469b597ab32d54e93edde09bd29a2451b88a
Joshua Shin <Josh@Joshua-Shins-MacBook-Pro.local>
2015-09-01 13:40:20 -0700

    added a couple of lines for real

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -25,14 +25,14 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* YOUR CODE HERE */;
+    return move || getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* YOUR CODE HERE */;
+    return move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
